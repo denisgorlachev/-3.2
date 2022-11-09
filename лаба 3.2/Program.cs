daa3b4e719ffd1534032ba93294ac360da24d1fd
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:45:15 +0200

    Добавьте файлы проекта.

---
+++ лаба 3.2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Горлачев средний уровень 
+Console.WriteLine("Введите число m: ");
+double m = double.Parse(Console.ReadLine());
+double sub = 0;
+for (int n = 1; n < 1000; n++)
+{
+    sub = Math.Pow(n, 2) - Math.Pow(m, 2);
+}
+Console.WriteLine("Ответ: " + sub);
